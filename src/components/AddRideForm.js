d9f1fc1c6d2d4c9cb7a6957d3e55d0ff854905b3
Alessandra Chardon <achardon@ucla.edu>
2022-01-17 15:15:34 -0500

    add new ride form

--- src/components/AddRideForm.js
+++ src/components/AddRideForm.js
@@ -4,7 +4,13 @@ import styled from 'styled-components';
 
 function AddRideForm() {
 
-    const [newRide, setNewRide] = useState([])
+    const [newRide, setNewRide] = useState({
+        name: "Alessandra",
+        date: '',
+        description: '',
+        route: 'East Orchard Loop'
+    })
+
     const [routes, setRoutes] = useState([])
 
     useEffect(() => {
@@ -17,57 +23,72 @@ function AddRideForm() {
 
     const routeArr = routes.map(r => r.title)
 
-    console.log(routeArr)
+    function handleChange(e) {
+        setNewRide({...newRide,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        console.log(newRide)
+    }
 
     return(
-        <Form>
+        <>
             <h1>Add Ride</h1>
-            <form>
-                <label>
-                    Name:
-                    <select>
-                        <option value="Alessandra">Alessandra</option>
-                        <option value="Patrick">Patrick</option>
-                    </select>
-                </label>
+            
+            <Form>
+                
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Name:
+                        <select name="name" value={newRide.name} onChange={handleChange}>
+                            <option value="Alessandra">Alessandra</option>
+                            <option value="Patrick">Patrick</option>
+                        </select>
+                    </label>
 
-                <label>
-                    Route:
-                    <select>
-                        {routeArr.map(route => {
-                            return <option key={route} value="route">{route}</option>
-                        })}
-                        <option value="other">Other</option>
-                    </select>
-                </label>
+                    <label>
+                        Route:
+                        <select name="route" value={newRide.route} onChange={handleChange}>
+                            {routeArr.map(route => {
+                                return <option key={route}>{route}</option>
+                            })}
+                            <option value="other">Other</option>
+                        </select>
+                    </label>
 
-                <label>
-                    Title:
-                    <input type="text" name="title" />
-                </label>
+                    <label>
+                        Title:
+                        <input type="text" name="title" value={newRide.title} onChange={handleChange}/>
+                    </label>
 
-                <label>
-                    Date:
-                    <input type="text" name="date" />
-                </label>
+                    <label>
+                        Date:
+                        <input type="text" name="date" value={newRide.date} onChange={handleChange}/>
+                    </label>
 
-                <label>
-                    Description:
-                    <input type="text" name="description" />
-                </label>
+                    <label>
+                        Description:
+                        <input type="text" name="description" value={newRide.description} onChange={handleChange}/>
+                    </label>
 
-                <input type="submit" value="Add" />
-            </form>
-        </Form >
+                    <input type="submit" value="Add" />
+                </form>
+            </Form >
+            
+        </>
     )
 }
 
 export default AddRideForm;
 
 const Form = styled.div`
+    display: flex;
     margin: auto;
     box-shadow: 4px 4px 8px 10px rgba(0,0,0,0.3);
-    width: 650px;
+    width: 600px;
     input {
         padding: 5px;
         margin: 5px;
