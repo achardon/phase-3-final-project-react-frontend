b2cf28aa402fee2c6561291ba4910dfbc6038da7
Alessandra Chardon <achardon@ucla.edu>
2022-01-21 10:42:53 -0500

    fetching goals from backend

--- src/components/Goals.js
+++ src/components/Goals.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import {useState, useEffect} from 'react';
 
 function Goals() {
 
-  return (
+    const [goals, setGoals] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:9292/goals')
+        .then(r => r.json())
+        .then(data => {
+            setGoals(data)
+        })
+    }, [])
+
+    console.log(goals)
+
+    //How do I use rider_name function from backend to get name to appear for each goal here? (instead of right now where it is just displaying the id?)
+
+    return (
 
       <div className = "rounded-lg py-4 shadow-xl ">
         <h1 className="bg-green-700 text-6xl py-4 text-center">Goals</h1>
         <div className='bg-green-500 p-8'>
-            Goals here
+            {goals.map(goal => <p key={goal.id}>{goal.goal} ({goal.rider_id})</p>)}
         </div>
 
       </div>
